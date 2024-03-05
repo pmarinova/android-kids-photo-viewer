d980c9ee842f152ab3c7c353273129fb78287c66
Plamena Marinova <plamena.marinova@gmail.com>
2024-03-05 06:29:13 +0200

    use view pager to swipe between images

--- app/src/main/java/pm/android/kidsphotoviewer/PhotosActivity.java
+++ app/src/main/java/pm/android/kidsphotoviewer/PhotosActivity.java
@@ -1,114 +1,92 @@
 package pm.android.kidsphotoviewer;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GestureDetectorCompat;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.core.view.WindowInsetsControllerCompat;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
-import com.bumptech.glide.Glide;
-
-import pm.android.kidsphotoviewer.databinding.ActivityPhotosBinding;
+import java.util.Objects;
 
 public class PhotosActivity extends AppCompatActivity {
 
-    private static final int MIN_SWIPING_DISTANCE = 50;
-    private static final int THRESHOLD_VELOCITY = 50;
+    private static class ViewPagerAdapter extends PagerAdapter {
 
-    private static int[] IMAGES = {
+        private final Context context;
+        private final int[] images;
+        private final LayoutInflater layoutInflater;
+
+        public ViewPagerAdapter(Context context, int[] images) {
+            this.context = context;
+            this.images = images;
+            layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        @Override
+        public int getCount() {
+            return images.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+            return view == ((LinearLayout)object);
+        }
+
+        @Override
+        @NonNull
+        public Object instantiateItem(@NonNull ViewGroup container, final int position) {
+            View itemView = layoutInflater.inflate(R.layout.photo_item, container, false);
+            ImageView imageView = (ImageView)itemView.findViewById(R.id.photo_image_view);
+            imageView.setImageResource(images[position]);
+            Objects.requireNonNull(container).addView(itemView);
+            return itemView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, @NonNull Object object) {
+            container.removeView((LinearLayout) object);
+        }
+    }
+
+    private static final int[] IMAGES = {
             R.drawable.bear,
             R.drawable.bird,
             R.drawable.cat,
             R.drawable.flamingo
     };
 
-    private ActivityPhotosBinding binding;
-
-    private ImageView imageView;
-
-    private int currentImageIndex;
-
-    private GestureDetectorCompat gestureDetector;
-
-    private GestureDetector.SimpleOnGestureListener gestureListener = new GestureDetector.SimpleOnGestureListener() {
-        @Override
-        public boolean onFling(@Nullable MotionEvent e1, @NonNull MotionEvent e2, float velocityX, float velocityY) {
-            if (e1.getX() - e2.getX() > MIN_SWIPING_DISTANCE && Math.abs(velocityX) > THRESHOLD_VELOCITY)
-            {
-                onSwipeLeft();
-                return false;
-            }
-            else if (e2.getX() - e1.getX() > MIN_SWIPING_DISTANCE && Math.abs(velocityX) > THRESHOLD_VELOCITY)
-            {
-                onSwipeRight();
-                return false;
-            }
-            return false;
-        }
-    };
+    private ViewPager viewPager;
+    private ViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photos);
 
+        hideSystemBars();
+
+        viewPager = (ViewPager)findViewById(R.id.photos_view_pager);
+        viewPagerAdapter = new ViewPagerAdapter(this, IMAGES);
+        viewPager.setAdapter(viewPagerAdapter);
+    }
+
+    private void hideSystemBars() {
         WindowInsetsControllerCompat windowInsetsController =
                 WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView());
-        
+
         windowInsetsController.setSystemBarsBehavior(
-            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
 
         windowInsetsController.hide(WindowInsetsCompat.Type.systemBars());
-
-        binding = ActivityPhotosBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        imageView = binding.imageView;
-        loadImage(0);
-
-        gestureDetector = new GestureDetectorCompat(this, gestureListener);
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if (gestureDetector.onTouchEvent(event)) {
-            return true;
-        }
-        return super.onTouchEvent(event);
-    }
-
-    private void onSwipeLeft() {
-        nextImage();
-    }
-
-    private void onSwipeRight() {
-        prevImage();
-    }
-
-    private void nextImage() {
-        currentImageIndex++;
-        if (currentImageIndex == IMAGES.length) {
-            currentImageIndex = 0;
-        }
-        loadImage(currentImageIndex);
-    }
-
-    private void prevImage() {
-        currentImageIndex--;
-        if (currentImageIndex == -1) {
-            currentImageIndex = IMAGES.length - 1;
-        }
-        loadImage(currentImageIndex);
-    }
-
-    private void loadImage(int index) {
-        Glide.with(this).load(IMAGES[index]).into(binding.imageView);
     }
 }
